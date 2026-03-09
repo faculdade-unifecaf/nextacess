05b19945337809bb1009e3c2ff3b0fd7d9024bad
lukinhasc-dev <lucas.sca2022@gmail.com>
2026-03-09 17:22:26 -0300

    build: configurado banco de dados para o backend, e chamada via axios para o frontend com services

---
+++ back-end/src/config/database.ts
@@ -0,0 +1,10 @@
+import postgres from 'postgres'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const connectionString = process.env.DATABASE_URL as string
+
+const sql = postgres(connectionString, { ssl: 'require' })
+
+export default sql
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,18 @@
 import app from "./app";
+import sql from "./config/database";
 
 const PORT = 3000
+
+async function testarConexao() {
+    try {
+        await sql`SELECT 1`;
+        console.log('✅ Conexão com o banco de dados estabelecida com sucesso!');
+    } catch (error) {
+        console.error('❌ Erro ao conectar com o banco de dados:', error);
+    }
+}
+
+testarConexao();
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado na porta ${PORT} 🚀`);
